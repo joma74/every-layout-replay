e3715627b9d8fd8c8d601a4727722f226c30fd89
joma74 <dev-mails@gmx.net>
2021-01-08 11:28:25 +0100

    fix: flex shorthand 1 does not set flex-basis to 0 but converts to 0px. Like 0%, also gives incorrect results in FF and Chrome

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,13 @@ module.exports = {
 				trueGray: colors.trueGray,
 				warmGray: colors.warmGray,
 			},
+			flex: {
+				/** Browser Bug
+				 * A flex basis of 0% is not interpreted by Browsers the same as 0.
+				 * Additionally, if set via composite flex: 1 0;, FF converts mistakenly that to flex 1 1 0px;
+				 * */
+				1: "1; flex-grow: 1; flex-shrink: 1; flex-basis: 0;",
+			},
 		},
 	},
 	variants: {
