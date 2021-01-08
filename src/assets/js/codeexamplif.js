bcb0974f181779c3259468295ad16a581752a54b
joma74 <dev-mails@gmx.net>
2021-01-08 18:51:29 +0100

    chore: Remove all attributes named FORCODEEXAMPLE_ATT_NAME

--- src/assets/js/codeexamplif.js
+++ src/assets/js/codeexamplif.js
@@ -1,11 +1,13 @@
 Prism = Prism || {}
 Prism.manual = true
+const FORCODEEXAMPLE_ATT_NAME = "for-code-example"
+/** @type {HTMLElement} */
 const codeExampleDom = document.getElementById("usage-example").cloneNode(true)
 const notCodingExampleElements = document.createNodeIterator(
 	codeExampleDom,
 	NodeFilter.SHOW_ALL,
 	(/** @type {Node} */ elm) => {
-		if (elm.nodeType == 3 || !elm.hasAttribute("for-code-example"))
+		if (elm.nodeType == 3 || !elm.hasAttribute(FORCODEEXAMPLE_ATT_NAME))
 			return NodeFilter.FILTER_ACCEPT
 		else return NodeFilter.FILTER_SKIP
 	},
@@ -17,6 +19,17 @@ while (
 ) {
 	notCodingExampleElement.parentElement.removeChild(notCodingExampleElement)
 }
+/**
+ *  Remove all attributes named FORCODEEXAMPLE_ATT_NAME
+ **/
+if (codeExampleDom.getAttribute(FORCODEEXAMPLE_ATT_NAME) != null) {
+	codeExampleDom.removeAttribute(FORCODEEXAMPLE_ATT_NAME)
+}
+codeExampleDom
+	.querySelectorAll(`[${FORCODEEXAMPLE_ATT_NAME}]`)
+	.forEach((elm) => {
+		elm.removeAttribute(FORCODEEXAMPLE_ATT_NAME)
+	})
 let codeExample = codeExampleDom.outerHTML
 codeExample = html_beautify(codeExample, {
 	inline: [],
